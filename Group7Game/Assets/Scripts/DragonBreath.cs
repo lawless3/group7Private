3ab693c3c08c3a48e991c3c17bf0e6d2315ac4d2
Ewan-Fawns <59920488+Ewan-Fawns@users.noreply.github.com>
2020-03-26 08:57:32 +0000

    Dragon Bar update
    Made it so the dragon bar gradually reduces instead of going to nothing immediately.

--- Group7Game/Assets/Scripts/DragonBreath.cs
+++ Group7Game/Assets/Scripts/DragonBreath.cs
@@ -9,29 +9,57 @@ public class DragonBreath : MonoBehaviour {
     public float timeInterval;
     private float timePassed = 0;
     public float stopShake;
+    private bool reduceBar = false;
+    private float currentShake = 0;
+    private float barReduction;
+    public float barReductionTime;
     public GameObject Fill;
 	// Use this for initialization
 	void Start () {
-		
+        barReduction = barReductionTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(timePassed >= timeInterval)
         {
+            reduceBar = true;
             transform.position = new Vector3(GameObject.Find("Character").transform.position.x - 60, GameObject.Find("Character").transform.position.y + 18, 0);
             timePassed = 0;
+            currentShake = 0;
             CinemachineVirtualCamera vcam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
             vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0.5f;
             
         }
-        if (timePassed >= stopShake)
+
+        if (currentShake < stopShake)
         {
-            CinemachineVirtualCamera vcam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
-            vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
+            currentShake += Time.deltaTime;
+            if (currentShake >= stopShake)
+            {
+                CinemachineVirtualCamera vcam = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();
+                vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = 0;
+            }
         }
-        timePassed += Time.deltaTime;
-        Fill.GetComponent<Image>().fillAmount = timePassed / timeInterval;
+
+        if(reduceBar == false)
+        {
+            timePassed += Time.deltaTime;
+            Fill.GetComponent<Image>().fillAmount = timePassed / timeInterval;
+        }
+        else
+        {
+            Fill.GetComponent<Image>().fillAmount = barReduction / barReductionTime;
+            if (barReduction <= 0)
+            {
+                barReduction = barReductionTime;
+                reduceBar = false;
+            }
+            barReduction -= Time.deltaTime;
+        }
+
+        currentShake += Time.deltaTime;
+        
 	}
 
     private void FixedUpdate()
